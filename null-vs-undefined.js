f68d917bce205465c0d0809e06be74fb94a8ed2e
iqbal <iqbaldb32@gmail.com>
2021-05-28 00:22:52 +0600

    null vs undefined added

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+// Undefined List
+
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1, num2) {
+//     sum =num1 + num2;
+//     // return sum; Must return
+// }
+// const result = add(12, 32);
+// console.log(result);
+
+// const iqbal = {name:"Iqbal", fatherName:"Abdul Halim", age: 23};
+// console.log(iqbal.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages = [22, 2, 21];
+console.log(ages[22]);
