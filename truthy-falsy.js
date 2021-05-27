2ab60cb784f12d9766720d4e48817e2d92c6edd4
iqbal <iqbaldb32@gmail.com>
2021-05-27 23:59:26 +0600

    truthy-falsy added

---
+++ truthy-falsy.js
@@ -0,0 +1,20 @@
+// Falsy List
+// const name = 0;
+// const name = "";
+// const name;(Undefined)
+// const name = null;
+// const name = NaN;
+
+// Truthy List
+// const name = "iqbal";
+// const name = " ";
+// const name = "0";
+// const name = [];
+
+const name = "Iqbal";
+if (name) {
+   console.log("condition is true") 
+} 
+else {
+    console.log("condition is false")
+}
