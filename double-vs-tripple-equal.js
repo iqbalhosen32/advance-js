a1dab340401612bac7e6f17a190db0a6bcdb2342
iqbal <iqbaldb32@gmail.com>
2021-05-28 19:43:10 +0600

    double vs tripple equal added

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,17 @@
+// when use double equal then they compare with just value
+// const a = 2;
+// const b = "2";
+// if (a == b) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+// when use tripple equal then they compare with value and type of value
+const a = 2;
+const b = "2";
+if (a === b) {
+    console.log('Condition is true');
+} else {
+    console.log('Condition is false');
+}
